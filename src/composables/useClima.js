e0e5c7e7a5360b988aa23e29c55a0bc7c30fc523
santiagopereyramarchetti <santiagopereyra2702@gmail.com>
2025-01-15 18:32:40 -0300

    https fixed

--- src/composables/useClima.js
+++ src/composables/useClima.js
@@ -17,7 +17,7 @@ export default function useClima(){
         try {
             cargando.value = true
 
-            const url = `http://api.openweathermap.org/geo/1.0/direct?q=${ciudad},${pais}&limit=1&appid=${key}`
+            const url = `https://api.openweathermap.org/geo/1.0/direct?q=${ciudad},${pais}&limit=1&appid=${key}`
 
             const { data } = await axios(url)
 
